776968d4197e1ae998a9565648b20f5c2b4d6592
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-07-29 21:40:58 +0530

    Create 110. Balanced Binary Tree.java

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int dfs(TreeNode root){
+        if(root==null)return 0;
+        int lh=dfs(root.left);
+        int rh=dfs(root.right);
+        if(lh==-1 || rh==-1)return -1;
+        if(Math.abs(lh-rh)>1)return -1;
+        return 1+Math.max(lh,rh);
+    }
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!=-1;
+    }
+}
