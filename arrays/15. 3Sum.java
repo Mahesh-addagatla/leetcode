08bac802efc13749d76691335c5dd35ff45bde72
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2025-03-14 15:59:22 +0530

    Create 15. 3Sum.java

---
+++ arrays/15. 3Sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                    k--;
+                }else if (sum<0){
+                    j++;
+                }else{
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
