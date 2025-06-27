91456f2d3938b76712dcd32a0834622a61b75096
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2025-06-27 10:56:31 +0530

    Create longest common substring.cpp

---
+++ dp/longest common substring.cpp
@@ -0,0 +1,27 @@
+int LCSubStr(string &str1, string &str2)
+{
+
+    // Write your code here
+    int n = str1.size();
+    int m = str2.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=0;i<=n;i++){
+        dp[i][0]=0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    int ans = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(str1[i-1]==str2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+                ans = max(ans,dp[i][j]);
+            }
+            else dp[i][j] = 0;
+        }
+    }
+    return ans;
+
+}
+
