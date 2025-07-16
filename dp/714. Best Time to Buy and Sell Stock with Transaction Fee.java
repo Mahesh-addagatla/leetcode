875e8a398d1ec60b3dd94e17389427682ed7530e
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2025-07-16 10:40:24 +0530

    Create 714. Best Time to Buy and Sell Stock with Transaction Fee.java

---
+++ dp/714. Best Time to Buy and Sell Stock with Transaction Fee.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int i=0;i<2;i++){
+            dp[0][i] = 0;
+        }
+        for(int i=n-1;i>=0;i--){
+            dp[i][1] = Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0] = Math.max(prices[i]+dp[i+1][1]-fee,dp[i+1][0]);
+        }
+        return dp[0][1];
+    }
+}
